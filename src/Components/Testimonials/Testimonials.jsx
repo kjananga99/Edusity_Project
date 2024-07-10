ec985a184704536de6155d5863eec8cfa7d2772d
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-11 01:39:26 +0530

    add slider option

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,10 +1,87 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testimonials.css'
+import next_icon from "../../assets/next-icon.png"
+import back_icon from "../../assets/back-icon.png"
+import user_1 from "../../assets/user-1.png"
+import user_2 from "../../assets/user-2.png"
+import user_3 from "../../assets/user-3.png"
+import user_4 from "../../assets/user-4.png"
+
 
 const Testimonials = () => {
+
+    const slider = useRef();
+    let tx = 0;
+
+    const slideForward = ()=>{
+        if(tx > -50){
+            tx -= 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+    const slideBackward = ()=>{
+        if(tx < 0){
+            tx += 25;
+        }
+        slider.current.style.transform = `translateX(${tx}%)`;
+    }
+
   return (
-    <div>
-      
+    <div className='testimonials'>
+      <img src={next_icon} alt="" className='next-btn' onClick={slideForward}/>
+      <img src={back_icon} alt="" className='back-btn' onClick={slideBackward}/>
+      <div className="slider">
+        <ul ref={slider}>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_1} alt="" />
+                        <div>
+                            <h3>William Jackson</h3>
+                            <span>Edusity, USA</span>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_2} alt="" />
+                        <div>
+                            <h3>William Jackson</h3>
+                            <span>Edusity, USA</span>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_3} alt="" />
+                        <div>
+                            <h3>William Jackson</h3>
+                            <span>Edusity, USA</span>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                </div>
+            </li>
+            <li>
+                <div className="slide">
+                    <div className="user-info">
+                        <img src={user_4} alt="" />
+                        <div>
+                            <h3>William Jackson</h3>
+                            <span>Edusity, USA</span>
+                        </div>
+                    </div>
+                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.</p>
+                </div>
+            </li>
+        </ul>
+      </div>
     </div>
   )
 }
