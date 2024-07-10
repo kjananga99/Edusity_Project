e9e1f803cbcc355d8677c5284b0bcbf4c43aae8c
jananga <kavinduwijewardhana1999@gmail.com>
2024-07-11 01:51:24 +0530

    add component - Contact

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+      <div className="contact-col">
+        <h3>Send us a message</h3>
+        <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.The point of using Lorem Ipsum is that it has a more-or-less normal.</p>
+        <ul>
+            <li>Contact@Janangawijewardhana</li>
+            <li>076-4846356</li>
+            <li>KALANA,KAVISIGAMUWA<br/> 6008 , UDAGAMA</li>
+        </ul>
+      </div>
+      <div className="contact-col"></div>
+    </div>
+  )
+}
+
+export default Contact
